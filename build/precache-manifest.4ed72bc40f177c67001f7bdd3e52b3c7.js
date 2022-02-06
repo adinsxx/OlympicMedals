8e5258f82de2d27f5cf1575225a6177d20f140c5
Connor <c.mccurrie@gmail.com>
2022-02-06 14:50:11 -0600

    adding build folder

---
+++ build/precache-manifest.4ed72bc40f177c67001f7bdd3e52b3c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfc999fef94fe8407db32cacd36062d3",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "7878ecd7d422984eaba5",
+    "url": "/react-gh-pages/static/css/main.d7dc1369.chunk.css"
+  },
+  {
+    "revision": "59eedd81c2459a8faae9",
+    "url": "/react-gh-pages/static/js/2.e8b6cb0c.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/react-gh-pages/static/js/2.e8b6cb0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52aff327f9afa36c0591",
+    "url": "/react-gh-pages/static/js/3.55665959.chunk.js"
+  },
+  {
+    "revision": "7878ecd7d422984eaba5",
+    "url": "/react-gh-pages/static/js/main.caaae14b.chunk.js"
+  },
+  {
+    "revision": "e49bd8a724b7254838e3",
+    "url": "/react-gh-pages/static/js/runtime-main.1a242ec0.js"
+  }
+]);
