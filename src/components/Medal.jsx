c73974d2a8d52ad1d8ef96f9edad4d69a143a172
Connor <c.mccurrie@gmail.com>
2022-02-19 19:03:18 -0600

    Axios added Handle add/delete hooked up to backend API

--- src/components/Medal.jsx
+++ src/components/Medal.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 const Medal = (props) => {
-    const {medal, country, onIncrement, onSubtract} = props;
+    const {medal, country, onIncrement, onDecrement} = props;
     return (
         <div className="medals">
             {medal.name} Medals: {country[medal.name]}
             <button onClick={() => onIncrement(country.id, medal.name)}>+</button>
-            <button disabled={country[medal.name] === 0} onClick={() => onSubtract(country.id, medal.name)}>-</button>
+            <button disabled={country[medal.name] === 0} onClick={() => onDecrement(country.id, medal.name)}>-</button>
         </div>
     );
 }
